04630d5da896d25ef1f94280b9efa98e7217e445
Michael Atlan <ton@email.com>
2026-02-13 00:11:04 +0100

    chore: nettoyage scripts temporaires

--- _cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/mikaatlan/Desktop/avis-doctolib
-git add -A
-git commit -m "chore: suppression script temporaire _commit.sh"
---
+++ _final.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/mikaatlan/Desktop/avis-doctolib
+git add -A
+git commit -m "chore: nettoyage scripts temporaires" --allow-empty
+rm -f /Users/mikaatlan/Desktop/avis-doctolib/_final.sh
